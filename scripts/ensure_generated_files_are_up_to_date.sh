fb72e864f8682ef8ccd33ac5923fd4b56e24151d
allsochen <erxiao.chen.gd@gmail.com>
2024-10-16 19:56:46 +0800

    feat: github actions

--- scripts/ensure_generated_files_are_up_to_date.sh
+++ scripts/ensure_generated_files_are_up_to_date.sh
@@ -1,9 +1,9 @@
 set -exu
 
 brew install jq
-scripts/update_contributors.sh
+#scripts/update_contributors.sh
 #scripts/import_l10n_strings_from_poeditor.sh
-scripts/extract_l10n_strings.sh
+#scripts/extract_l10n_strings.sh
 # pod install
 
 git status
